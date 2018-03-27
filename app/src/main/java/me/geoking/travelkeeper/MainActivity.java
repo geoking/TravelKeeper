dbb072ae4331a14f37d6edf739791105a92ce3e4
George King <geo.king.me@gmail.com>
2018-03-27 17:59:51 +0100

    Update edit holiday screen, added menu icon

--- app/src/main/java/me/geoking/travelkeeper/MainActivity.java
+++ app/src/main/java/me/geoking/travelkeeper/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.holiday_details_confirm, menu);
         return true;
     }
 
